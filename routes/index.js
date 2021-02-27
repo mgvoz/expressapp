b4cf1e60d3f9d42de7bfad1a9d4e086363c62cb6
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2021-02-27 13:14:25 -0500

    import hasAuth

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 let landing = require('../controllers/landing');
 let user = require('../controllers/user');
+let { isLoggedIn, hasAuth } = require('../middleware/hasAuth');
 
 router.get('/login', user.show_login);
 router.get('/signup', user.show_signup);
